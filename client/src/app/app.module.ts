1aacb1b5deaa8d834b44ff053bc5032edd401d5c
Jason Pham <vinhphuong0206@gmail.com>
2020-05-27 23:15:11 +1000

    Routing and Lazy Loading added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,8 +18,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
-    SharedModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
