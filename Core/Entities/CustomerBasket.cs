34ffea03de802b895591af838cc3a1704f8ce37a
Jason Ph <vinhphuong0206@gmail.com>
2020-07-04 21:26:22 +1000

    Added server validation

--- Core/Entities/CustomerBasket.cs
+++ Core/Entities/CustomerBasket.cs
@@ -5,12 +5,10 @@ namespace Core.Entities
     public class CustomerBasket
     {
         public CustomerBasket() { }
-
         public CustomerBasket(string id)
         {
             Id = id;
         }
-
         public string Id { get; set; }
         public List<BasketItem> Items { get; set; } = new List<BasketItem>();
     }
