a097545819b779a2f72a9d3bf9e32c1176eb2bca
Jason Ph <vinhphuong0206@gmail.com>
2020-07-12 15:51:11 +1000

    Almost ready for publishing

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -6,7 +6,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { CdkStepperModule } from '@angular/cdk/stepper';
 import { StepperComponent } from './components/stepper/stepper.component';
@@ -30,6 +30,7 @@ import { RouterModule } from '@angular/router';
     ReactiveFormsModule,
     CdkStepperModule,
     RouterModule,
+    FormsModule,
   ],
   exports: [
     PaginationModule,
@@ -43,6 +44,7 @@ import { RouterModule } from '@angular/router';
     CdkStepperModule,
     StepperComponent,
     BasketSummaryComponent,
+    FormsModule,
   ],
 })
 export class SharedModule {}
